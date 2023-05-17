f3179ca543d2eff3c8a8eaf1d88375a759ac51a4
kilnonedre <1600513615@qq.com>
2023-05-17 09:13:02 +0800

    测试

--- pages/load/index.tsx
+++ pages/load/index.tsx
@@ -1,18 +1,20 @@
-import React, { useRef, useEffect } from "react";
-import styles from "./loadStyle.module.scss";
+import React, { useRef, useEffect } from 'react'
+import styles from './loadStyle.module.scss'
+
+const test = 1
 
 const Loading = () => {
-    const canvasRef = useRef<HTMLCanvasElement>(null);
-    return (
-        <div className={styles["panel"]}>
-            <div>loading</div>
-            <canvas ref={canvasRef} width="600px" height="600px"></canvas>
-        </div>
-    );
-};
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+  return (
+    <div className={styles['panel']}>
+      <div>loading</div>
+      <canvas ref={canvasRef} width="600px" height="600px"></canvas>
+    </div>
+  )
+}
 
-export default Loading;
+export default Loading
 
 Loading.getLayout = (page: Document) => {
-    return <>{page}</>;
-};
+  return <>{page}</>
+}
