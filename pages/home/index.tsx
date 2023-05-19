c04f74c05533f2e8b4b98ea56b99f57d49a889d1
kilnonedre <1600513615@qq.com>
2023-05-19 09:13:49 +0800

    feat: 轮播图静态页面

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -5,30 +5,30 @@ import Carousel from '@/views/home/Carousel'
 const titleList = [
   {
     name: 'New & Trendy',
-    component: <Carousel />
+    component: <Carousel />,
   },
   {
     name: 'Now Trending',
-    component: 1
+    component: 1,
   },
   {
     name: 'Short Reads',
-    component: 1
+    component: 1,
   },
   {
     name: 'Blog',
-    component: 1
-  }
+    component: 1,
+  },
 ]
 
 const Home = () => {
   return (
     <>
-      <div>
+      <div className={styles['home']}>
         {titleList.map((title, index) => {
           return (
-            <div key={index}>
-              <div className={styles['title']}>{title.name}</div>
+            <div className={styles['home-module']} key={index}>
+              <div className={styles['home-module-theme']}>{title.name}</div>
               {title.component}
             </div>
           )
--- views/home/Carousel/index.tsx
+++ views/home/Carousel/index.tsx
@@ -1,12 +1,96 @@
 import React from 'react'
+import styles from './carouselStyle.module.scss'
+import img from '@/assets/images/avatar/kilnoneblogAvatar.jpg'
 
 const Carousel = () => {
   return (
-    <div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+    <div className={styles['carousel']}>
+      <div className={styles['carousel-image']}>
+        <img src={img.src} alt="" />
+      </div>
+      <div className={styles['carousel-text']}>
+        <div className={styles['carousel-text-outer']}>
+          <div className={styles['carousel-text-outer-msg']}>
+            <p className={styles['carousel-text-outer-msg-subtype']}>Fantasy</p>
+            <p className={styles['carousel-text-outer-msg-divide']}></p>
+            <p className={styles['carousel-text-outer-msg-release']}>
+              1 Month Ago
+            </p>
+          </div>
+          <h1 className={styles['carousel-text-outer-title']}>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae,
+            quos.
+          </h1>
+          <p className={styles['carousel-text-outer-content']}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Exercitationem accusantium quasi quisquam id veritatis sit dolore
+            excepturi eius in porro nobis enim repellat assumenda earum vel
+            facilis, nemo mollitia! Voluptatibus, iusto quos saepe, nisi tempora
+            nobis voluptas voluptates aspernatur necessitatibus, beatae officiis
+            ullam debitis. Mollitia est quod fuga, quae commodi repellendus
+            accusantium minima obcaecati nisi provident pariatur magni dolores.
+            Perferendis, architecto? Ipsam amet consequatur totam fuga harum cum
+            vitae excepturi nihil illum fugit exercitationem similique,
+            perferendis provident odit eligendi quos, sapiente ab nulla
+            temporibus rem inventore. Voluptate nulla aut ipsam atque molestiae
+            ex fugit voluptatum, quibusdam, quod commodi doloribus dolores?
+          </p>
+          <div className={styles['carousel-text-outer-misc']}>
+            <div className={styles['carousel-text-outer-misc-reader']}>
+              12 Min Read
+            </div>
+            <div className={styles['carousel-text-outer-misc-show']}>
+              Read Full{' '}
+              <span className={styles['carousel-text-outer-misc-show-arrow']}>
+                
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles['carousel-text']}>
+        <div className={styles['carousel-text-outer']}>
+          <div className={styles['carousel-text-outer-msg']}>
+            <p className={styles['carousel-text-outer-msg-subtype']}>Fantasy</p>
+            <p className={styles['carousel-text-outer-msg-divide']}></p>
+            <p className={styles['carousel-text-outer-msg-release']}>
+              1 Month Ago
+            </p>
+          </div>
+          <h1 className={styles['carousel-text-outer-title']}>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae,
+            quos.
+          </h1>
+          <p className={styles['carousel-text-outer-content']}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Exercitationem accusantium quasi quisquam id veritatis sit dolore
+            excepturi eius in porro nobis enim repellat assumenda earum vel
+            facilis, nemo mollitia! Voluptatibus, iusto quos saepe, nisi tempora
+            nobis voluptas voluptates aspernatur necessitatibus, beatae officiis
+            ullam debitis. Mollitia est quod fuga, quae commodi repellendus
+            accusantium minima obcaecati nisi provident pariatur magni dolores.
+            Perferendis, architecto? Ipsam amet consequatur totam fuga harum cum
+            vitae excepturi nihil illum fugit exercitationem similique,
+            perferendis provident odit eligendi quos, sapiente ab nulla
+            temporibus rem inventore. Voluptate nulla aut ipsam atque molestiae
+            ex fugit voluptatum, quibusdam, quod commodi doloribus dolores?
+          </p>
+          <div className={styles['carousel-text-outer-misc']}>
+            <div className={styles['carousel-text-outer-misc-reader']}>
+              12 Min Read
+            </div>
+            <div className={styles['carousel-text-outer-misc-show']}>
+              Read Full{' '}
+              <span className={styles['carousel-text-outer-misc-show-arrow']}>
+                
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles['carousel-image']}>
+        <img src={img.src} alt="" />
+      </div>
     </div>
   )
 }
