b776941e6cd93a1b5f32221360fda9dda8e4c2f3
kilnonedre <1600513615@qq.com>
2023-06-13 17:13:12 +0800

    ci: nextUi 组件库的引入

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,18 +2,30 @@ import '@/styles/formatStyle.scss'
 import type { AppProps } from 'next/app'
 import Header from '@/views/layout/header'
 import Footer from '@/views/layout/footer'
+import { NextUIProvider } from '@nextui-org/react'
+import { useSSR } from '@nextui-org/react'
 
 const App = ({ Component, pageProps }: AppProps) => {
+  const { isBrowser } = useSSR()
+
   if ((Component as any).getLayout) {
-    return (Component as any).getLayout(<Component {...pageProps} />)
+    return (
+      isBrowser && (
+        <NextUIProvider>
+          {(Component as any).getLayout(<Component {...pageProps} />)}
+        </NextUIProvider>
+      )
+    )
   }
 
   return (
-    <>
-      <Header />
-      <Component {...pageProps} />
-      <Footer />
-    </>
+    isBrowser && (
+      <NextUIProvider>
+        <Header />
+        <Component {...pageProps} />
+        <Footer />
+      </NextUIProvider>
+    )
   )
 }
 
