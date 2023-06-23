04cf970b56faf5261cd5225022618a0866c19213
kilnonedre <1600513615@qq.com>
2023-06-23 12:04:21 +0800

    perf: layout 布局函数

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,5 @@
 import '@/styles/formatStyle.scss'
 import type { AppProps } from 'next/app'
-import Header from '@/views/layout/header'
-import Footer from '@/views/layout/footer'
 import { NextUIProvider } from '@nextui-org/react'
 import { useSSR } from '@nextui-org/react'
 
@@ -21,9 +19,7 @@ const App = ({ Component, pageProps }: AppProps) => {
   return (
     isBrowser && (
       <NextUIProvider>
-        <Header />
         <Component {...pageProps} />
-        <Footer />
       </NextUIProvider>
     )
   )
